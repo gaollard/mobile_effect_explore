77af3c0ab74d860d3b17cbbf61a38cca65c9b2c1
gaollard <gaollard@foxmail.com>
2018-10-07 22:50:11 +0800

    update

--- 02-swiper/swiper.js
+++ 02-swiper/swiper.js
@@ -19,6 +19,10 @@ function getTranslate (element) {
   }
 }
 
+function setTransition (element, duration) {
+	element.style['transition-duration'] = duration + 'ms'
+}
+
 function Swiper (container, options) {
 	this.param = options
 	this.$container = document.querySelector(container)
@@ -62,6 +66,7 @@ Swiper.prototype._initStyle = function () {
 	this.translate = 0
 	this.minTranslate = 0
 	this.maxTranslate = containerWidth * (length - 1)
+	this.isAnimating = false
 	slideMain.style.width = containerWidth * length + 'px'
 	slideMain.style.transform = 'translate3d(0px, 0px, 0px)'
 	slideList.forEach((slide) => {
@@ -73,20 +78,32 @@ Swiper.prototype._initEvents = function () {
 	this.$container.addEventListener('touchstart', this._onTouchStart.bind(this), false)
 	this.$container.addEventListener('touchmove', this._onTouchMove.bind(this), false)
 	this.$container.addEventListener('touchend', this._onToucEnd.bind(this), false)
+	this.$swiperMain.addEventListener('transitionend', this._onTransitionEnd.bind(this), false)
 }
 
 Swiper.prototype._updateTranslate = function (newTranslate) {
 	setTranslate(this.$swiperMain, newTranslate)
 }
 
+Swiper.prototype._updateTransition = function (duration) {
+	setTransition(this.$swiperMain, duration)
+}
+
 Swiper.prototype._onTouchStart = function (event) {
 	var point = event['changedTouches'][0]
+	if (this.isAnimating) {
+		return
+	}
 	this.touchStartPoint = point['pageX']
 	this.touchStartTime = +new Date()
 	this.touchStartTranslate = getTranslate(this.$swiperMain)['x']
+	this._updateTransition(0)
 }
 
 Swiper.prototype._onTouchMove = function (event) {
+	if (this.isAnimating) {
+		return
+	}
 	var point = event['changedTouches'][0]
 	var diff = point['pageX'] - this.touchStartPoint
 	var newTranslate = this.touchStartTranslate + diff
@@ -95,6 +112,9 @@ Swiper.prototype._onTouchMove = function (event) {
 }
 
 Swiper.prototype._onToucEnd = function (event) {
+	if (this.isAnimating) {
+		return
+	}
 	var newIndex = Math.round(-this.translate / this.width)
 	var translate = newIndex * this.width
 	if (translate < this.minTranslate) {
@@ -103,7 +123,13 @@ Swiper.prototype._onToucEnd = function (event) {
 	if (translate > this.maxTranslate) {
 		translate = this.maxTranslate
 	}
+	this._updateTransition(400)
+	this.isAnimating = true
 	this._updateTranslate(-translate)
+}
+
+Swiper.prototype._onTransitionEnd = function () {
+	this.isAnimating = false
 }
 
 Swiper.prototype.resize = function () {
